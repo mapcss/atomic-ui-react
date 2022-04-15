5ca84579fddd88ad4427420ce1f9813c2cb507a7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-15 16:42:29 +0900

    fix: fix detect browser env

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@
 
 export { isFunction } from "https://deno.land/x/isx@v1.0.0-beta.17/mod.ts";
 export type VFn = () => void;
-export const isBrowser = "window" in globalThis;
+export const isBrowser = !("Deno" in globalThis);
