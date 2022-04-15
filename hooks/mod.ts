a6f3c2affa589fba7fa0303463615e9d1cfebaa0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-15 23:14:33 +0900

    refactor(hooks): add submodule entry point

---
+++ hooks/mod.ts
@@ -0,0 +1,8 @@
+// This module is browser compatible.
+
+export { default as useBoolean } from "./use_boolean.ts";
+export type {
+  Param as UseBooleanParam,
+  ReturnValue as UseBooleanReturnValue,
+} from "./use_boolean.ts";
+export { default as useIsomorphicLayoutEffect } from "./use_isomorphic_layout_effect.ts";
--- mod.ts
+++ mod.ts
@@ -2,11 +2,5 @@
 
 export * from "./switch/mod.ts";
 export * from "./transition/mod.ts";
-export * from './tab/mod.ts'
-
-export { default as useBoolean } from "./hooks/use_boolean.ts";
-export type {
-  Param as UseBooleanParam,
-  ReturnValue as UseBooleanReturnValue,
-} from "./hooks/use_boolean.ts";
-export { default as useIsomorphicLayoutEffect } from "./hooks/use_isomorphic_layout_effect.ts";
+export * from "./tab/mod.ts";
+export * from "./hooks/mod.ts";
