79b9aa2038b3c9bc031284a9d80c71c512789345
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-16 16:06:49 +0900

    feat(tab): add searching recursive children to traverse

--- tab/traverse.ts
+++ tab/traverse.ts
@@ -1,34 +1,52 @@
-import { Children, isValidElement, ReactElement, ReactNode } from "react";
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+} from "react";
 import { isFunction } from "../deps.ts";
-import { isTab, isTabElement, isTabPanel } from "./assert.ts";
+import { hasChildren, isTab, isTabElement, isTabPanel } from "./assert.ts";
 import { TabElement } from "./types.ts";
 
 export type VisitOn = {
-  tab: (tabElement: TabElement) => ReactNode | undefined;
-  tabPanel: (tabElement: TabElement) => ReactNode | undefined;
+  tab: (tabElement: TabElement) => ReactElement | undefined;
+  tabPanel: (tabElement: TabElement) => ReactElement | undefined;
 };
 
 export function visit(
   children: Readonly<ReactNode>,
   { tab, tabPanel }: Readonly<Partial<VisitOn>>,
 ): ReactNode {
-  const parent = (child: ReactElement) => {
-    if (isTabElement(child)) {
-      if (isTab(child.type)) {
-        if (!isFunction(tab)) return child;
-        return tab(child);
-      } else if (isTabPanel(child.type)) {
-        if (!isFunction(tabPanel)) return child;
-        return tabPanel(child);
-      } else {
-        return child;
-      }
-    } else child;
+  const applyVisit = (child: TabElement): ReactElement | undefined => {
+    if (isTab(child.type)) {
+      if (!isFunction(tab)) return child;
+      return tab(child);
+    }
+    if (isTabPanel(child.type)) {
+      if (!isFunction(tabPanel)) return child;
+      return tabPanel(child);
+    }
+    return child;
   };
 
   return Children.map(children, (child) => {
     if (!isValidElement(child)) return child;
 
-    return parent(child);
+    const isTabComponentParent = isTabElement(child);
+    const parent = isTabComponentParent ? applyVisit(child) : child;
+
+    if (!parent) {
+      return;
+    }
+
+    const element = hasChildren(parent)
+      ? (() => {
+        const children = visit(child.props.children, { tab, tabPanel });
+        return cloneElement(parent, {}, children);
+      })()
+      : parent;
+
+    return element;
   });
 }
