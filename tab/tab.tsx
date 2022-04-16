50d5358cb6b7720deb72316277f69ea3e8629df7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-16 17:14:05 +0900

    fix(tab): rename `Tab` component props of `tag` to `as`

--- tab/tab.tsx
+++ tab/tab.tsx
@@ -4,11 +4,11 @@ import { createElement } from "react";
 import { TAB, TYPE } from "./constant.ts";
 import { useTabAttribute } from "./use_attribute.ts";
 
-type _Props<Tag extends keyof JSX.IntrinsicElements> = {
+type _Props<As extends keyof JSX.IntrinsicElements> = {
   /** The element tag as.
    * @default `button`
    */
-  tag?: Tag;
+  as?: As;
 
   /**
    * @default false
@@ -21,11 +21,11 @@ export type Props<Tag extends keyof JSX.IntrinsicElements = "button"> =
   & Omit<JSX.IntrinsicElements[Tag], keyof _Props<Tag>>;
 
 export default function Tab(
-  { tag = "button", isSelected = false, ...rest }: Props,
+  { as = "button", isSelected = false, ...rest }: Props,
 ): JSX.Element {
   const attribute = useTabAttribute({ isSelected });
 
-  return createElement(tag, { ...attribute, ...rest });
+  return createElement(as, { ...attribute, ...rest });
 }
 
 Tab[TYPE] = TAB;
