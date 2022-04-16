3f11983f92fb36e051ce5a4e69875eb48325bca8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-16 23:00:08 +0900

    feat(tab): add `renderProps` props that dynamic rendering props with context

--- tab/tab.tsx
+++ tab/tab.tsx
@@ -1,6 +1,6 @@
 // This module is browser compatible.
 
-import { createElement, useEffect, useRef } from "react";
+import { createElement, useEffect, useMemo, useRef } from "react";
 import { TAB, TYPE } from "./constant.ts";
 import { useTabAttribute } from "./use_attribute.ts";
 
@@ -15,24 +15,39 @@ type _Props<As extends keyof JSX.IntrinsicElements> = {
   tabPanelId?: string;
 
   focus?: boolean;
+
+  /** Dynamic rendering props with context. */
+  renderProps?: (
+    context: { isSelected: boolean },
+  ) => Omit<JSX.IntrinsicElements[As], keyof _Props<As>>;
 };
 
-export type Props<Tag extends keyof JSX.IntrinsicElements = "button"> =
-  & _Props<Tag>
-  & Omit<JSX.IntrinsicElements[Tag], keyof _Props<Tag>>;
+export type Props<As extends keyof JSX.IntrinsicElements = "button"> =
+  & _Props<As>
+  & Omit<JSX.IntrinsicElements[As], keyof _Props<As>>;
 
-export default function Tab(
-  { as = "button", isSelected, tabPanelId, focus, ...rest }: Props,
+export default function Tab<As extends keyof JSX.IntrinsicElements = "button">(
+  { as, isSelected, tabPanelId, focus, renderProps, ...rest }: Props<As>,
 ): JSX.Element {
   const ref = useRef<HTMLElement>(null);
+
+  const props = useMemo(
+    () => renderProps?.({ isSelected: isSelected ?? false }) ?? {},
+    [isSelected],
+  );
+
   useEffect(() => {
     if (!ref.current || !isSelected || !focus) return;
     ref.current.focus();
   }, [isSelected, focus]);
-
   const attribute = useTabAttribute({ isSelected, tabPanelId });
 
-  return createElement(as, { ref, ...attribute, ...rest });
+  return createElement(as ?? "button", {
+    ref,
+    ...attribute,
+    ...rest,
+    ...props,
+  });
 }
 
 Tab[TYPE] = TAB;
