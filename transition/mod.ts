5e02d9196bc65d1c6ab2ef83cfcc3b91c5b3da3e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-18 21:43:59 +0900

    chore(transition): remove unused, delete export

--- transition/mod.ts
+++ transition/mod.ts
@@ -1,7 +1,5 @@
 // This module is browser compatible.
 
-export { default as Transition } from "./transition.ts";
-export type { Props as TransitionProps } from "./transition.ts";
 export { default as TransitionProvider } from "./transition_provider.ts";
 export type { Props as TransitionProviderProps } from "./transition_provider.ts";
 
