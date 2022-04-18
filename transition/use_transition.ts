6461c844d3d65b927ff223820e9d1b8b6bfff426
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-18 17:32:36 +0900

    refactor(transition): split constant, types, hooks

--- transition/use_transition.ts
+++ transition/use_transition.ts
@@ -1,77 +1,16 @@
 // This module is browser compatible.
 
-import { DependencyList, useEffect, useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { RefObject, useMemo, useState } from "react";
+import { joinChars } from "../deps.ts";
+import { Transition, TransitionLifecycleMap } from "./types.ts";
+import useTransitionTiming from "./use_transition_timing.ts";
 import {
-  Callable,
-  evaluate,
-  filterTruthy,
-  joinChars,
-  ValueOf,
-} from "../deps.ts";
-import useOnMount from "../hooks/use_on_mount.ts";
-import { getDuration } from "./util.ts";
-
-type Transition =
-  | "enterFrom"
-  | "enterTo"
-  | "enter"
-  | "leaveFrom"
-  | "leaveTo"
-  | "leave";
-
-type TimingProps = {
-  from: Transition;
-  to: Transition;
-  via: Transition;
-};
-
-function useTiming(
-  { extension }: { extension: Callable<number> },
-  deps?: DependencyList,
-) {
-  const [state, next] = useState<Stage>(0);
-
-  useOnMount({
-    onBeforeMount: () => {
-      next(0);
-    },
-    onAfterMount: () => {
-      next(1);
-
-      let tid: number;
-
-      const rid = requestAnimationFrame(() => {
-        const ms = evaluate(extension);
-        tid = setTimeout(() => {
-          next(3);
-        }, ms);
-
-        next(2);
-      });
-
-      return () => {
-        cancelAnimationFrame(rid);
-        if (tid) {
-          clearTimeout(tid);
-        }
-      };
-    },
-  }, { deps });
-
-  const timing = useMemo(() => {
-    return LABEL[state];
-  }, [state]);
-
-  return timing;
-}
-
-const LABEL = {
-  0: "beforeMount",
-  1: "mount",
-  2: "afterMount",
-  3: "complete",
-} as const;
+  getDuration,
+  getTransitionMap,
+  mapTiming2TransitionLifecycle,
+} from "./util.ts";
+import { BEFORE_MOUNT, COMPLETE } from "./constant.ts";
 
 export type Param<T extends Element> = {
   ref: RefObject<T | undefined>;
@@ -86,8 +25,6 @@ export type ReturnValue = {
 
 export type TransitionProps = Record<Transition, string>;
 
-type Stage = 0 | 1 | 2 | 3;
-
 export default function useTransition<T extends Element>(
   { ref, show }: Readonly<Param<T>>,
   transitionProps: Readonly<
@@ -96,33 +33,36 @@ export default function useTransition<T extends Element>(
 ): ReturnValue {
   const [state, setState] = useState<boolean>(show);
 
-  const timing = useTiming({
+  const timing = useTransitionTiming({
     extension: () => {
       return ref.current ? getDuration(ref.current) : 0;
     },
   }, [show]);
 
   useEffect(() => {
-    if (timing === "beforeMount" && show) {
+    if (timing === BEFORE_MOUNT && show) {
       setState(true);
     }
   }, [timing]);
 
   useLayoutEffect(() => {
-    if (timing === "complete" && !show) {
+    if (timing === COMPLETE && !show) {
       setState(false);
     }
   }, [timing]);
 
-  const currentTransitions = useMemo<Transition[]>(() => {
-    const mapped = classNameMap(show);
-    return filterTruthy(mapCurrentClassNames(timing, mapped));
-  }, [timing, show]);
+  const transitionLifecycleMap = useMemo<TransitionLifecycleMap>(
+    () => getTransitionMap(show),
+    [show],
+  );
+
+  const currentTransitions = useMemo<Transition[]>(
+    () => mapTiming2TransitionLifecycle(timing, transitionLifecycleMap),
+    [timing, transitionLifecycleMap],
+  );
 
   const className = useMemo<string>(() => {
-    const transitions = filterTruthy(currentTransitions).map((key) =>
-      transitionProps[key]
-    );
+    const transitions = currentTransitions.map((key) => transitionProps[key]);
     return joinChars(transitions, " ");
   }, [currentTransitions]);
 
@@ -130,35 +70,5 @@ export default function useTransition<T extends Element>(
     className,
     show: state,
     currentTransitions,
-  };
-}
-
-export function mapCurrentClassNames(
-  stage: ValueOf<typeof LABEL>,
-  { from, via, to }: TimingProps,
-): (Transition | undefined)[] {
-  switch (stage) {
-    case "beforeMount": {
-      return [from];
-    }
-    case "mount": {
-      return [from, via];
-    }
-    case "afterMount": {
-      return [to, via];
-    }
-    default: {
-      return [];
-    }
-  }
-}
-
-export function classNameMap(
-  isEnter: boolean,
-): TimingProps {
-  return {
-    from: isEnter ? "enterFrom" : "leaveFrom",
-    to: isEnter ? "enterTo" : "leaveTo",
-    via: isEnter ? "enter" : "leave",
   };
 }
--- transition/util.ts
+++ transition/util.ts
@@ -1,3 +1,16 @@
+import {
+  AFTER_MOUNT,
+  BEFORE_MOUNT,
+  ENTER_TRANSITION_MAP,
+  LEAVE_TRANSITION_MAP,
+  MOUNT,
+} from "./constant.ts";
+import {
+  Transition,
+  TransitionLifecycleMap,
+  TransitionTiming,
+} from "./types.ts";
+
 /** Compute transition duration from `CSSStyleDeclaration` */
 export function getDuration(el: Element): number {
   try {
@@ -9,5 +22,29 @@ export function getDuration(el: Element): number {
     return num * 1000;
   } catch {
     return 0;
+  }
+}
+
+export function getTransitionMap(isEnter: boolean): TransitionLifecycleMap {
+  return isEnter ? ENTER_TRANSITION_MAP : LEAVE_TRANSITION_MAP;
+}
+
+export function mapTiming2TransitionLifecycle(
+  transitionTiming: TransitionTiming,
+  { init, start, middle, end }: TransitionLifecycleMap,
+): Transition[] {
+  switch (transitionTiming) {
+    case BEFORE_MOUNT: {
+      return init;
+    }
+    case MOUNT: {
+      return start;
+    }
+    case AFTER_MOUNT: {
+      return middle;
+    }
+    default: {
+      return end;
+    }
   }
 }
