222cfdf5880c5ef843365ff15c637e8b80b7c3c9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-26 11:16:49 +0900

    feat(transition): add `cleanTransitionProps` to return

--- transition/use_transition.ts
+++ transition/use_transition.ts
@@ -1,6 +1,7 @@
 // This module is browser compatible.
 
 import { DependencyList, RefObject, useMemo } from "react";
+import { isLength0, mapValues } from "../deps.ts";
 import { isRefObject, Lazyable, lazyEval } from "../util.ts";
 import useIsFirstMount from "../hooks/use_is_first_mount.ts";
 import useMutated from "../hooks/use_mutated.ts";
@@ -55,6 +56,11 @@ export type ReturnValue =
     /** Whether transition lifecycle is completed or not. */
     isCompleted: boolean;
 
+    /** Non-duplicated token and space transition props
+     * It guarantee that there is no empty string or spaces only characters.
+     */
+    cleanTransitionProps: Partial<TransitionProps>;
+
     /** List of currently adapted transition. */
     currentTransitions: Transition[];
     /** Current transition lifecycle */
@@ -91,10 +97,11 @@ export default function useTransition<T extends Element>(
   deps: DependencyList,
 ): ReturnValue {
   const { isFirstMount } = useIsFirstMount();
+  const transitionPropsStr = JSON.stringify(transitionProps);
   const hasMutate = useMutated([
     isShow,
     immediate,
-    JSON.stringify(transitionProps),
+    transitionPropsStr,
   ]);
 
   const use = useMemo<boolean>(() => {
@@ -124,6 +131,11 @@ export default function useTransition<T extends Element>(
     [isShow],
   );
 
+  const cleanTransitionProps = useMemo<Partial<TransitionProps>>(
+    () => cleanRecordToken(transitionProps),
+    [transitionPropsStr],
+  );
+
   const currentTransitions = useMemo<Transition[]>(
     () =>
       isActivated === false ? [] : transitionLifecycleMap[transitionLifecycle],
@@ -141,9 +153,14 @@ export default function useTransition<T extends Element>(
   );
 
   const classNames = useMemo<string[]>(() => {
-    const transitions = currentTransitions.map((key) => transitionProps[key]);
+    const transitions = currentTransitions.map((key) =>
+      cleanTransitionProps[key]
+    );
     return cleanTokens(transitions);
-  }, [JSON.stringify(currentTransitions)]);
+  }, [
+    JSON.stringify(currentTransitions),
+    JSON.stringify(cleanTransitionProps),
+  ]);
 
   const className = useMemo<string | undefined>(() => {
     const _className = classNames.join(" ");
@@ -159,6 +176,7 @@ export default function useTransition<T extends Element>(
     status,
     currentTransitions,
     lifecycle: transitionLifecycle as never, // for conditional types,
+    cleanTransitionProps,
   };
 }
 
@@ -170,4 +188,14 @@ export function resolveElement<E extends Element>(
   }
 
   return lazyEval(elementLike);
+}
+
+export function cleanRecordToken<
+  T extends Readonly<Record<PropertyKey, string | undefined>>,
+>(record: T): T {
+  return mapValues(record, (value) => {
+    if (!value) return;
+    const cleanedTokens = cleanTokens([value]);
+    return isLength0(cleanedTokens) ? undefined : cleanedTokens.join(" ");
+  }) as T;
 }
