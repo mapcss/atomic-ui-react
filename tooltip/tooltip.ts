5d024e2436e7fbc32912197f1d4b89c775eb334f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-04-30 20:57:20 +0900

    feat(tooltip): export `TooltipProps` types

--- tooltip/tooltip.ts
+++ tooltip/tooltip.ts
@@ -1,12 +1,29 @@
 // This module is browser compatible.
 
-import { createElement, forwardRef } from "react";
+import { createElement, ForwardedRef, forwardRef, useMemo } from "react";
 
-const Tooltip = forwardRef<HTMLSpanElement, JSX.IntrinsicElements["span"]>(
-  (props, ref) => {
-    const role = "tooltip";
-    return createElement("span", { role, ref, ...props });
-  },
-);
+declare module "react" {
+  // deno-lint-ignore ban-types
+  function forwardRef<T, P = {}>(
+    render: (props: P, ref: Ref<T>) => ReactElement | null,
+  ): (props: P & RefAttributes<T>) => ReactElement | null;
+}
+
+export type Props<As extends keyof JSX.IntrinsicElements> = {
+  /**
+   * @default `span`
+   */
+  as?: As;
+} & JSX.IntrinsicElements[As];
+
+function _Tooltip<As extends keyof JSX.IntrinsicElements = "span">(
+  { as: _as, ...props }: Props<As>,
+  ref: ForwardedRef<As>,
+) {
+  const as = useMemo<string>(() => _as ?? "span", [_as]);
+  return createElement(as, { role: "tooltip", ref, ...props });
+}
+
+const Tooltip = forwardRef(_Tooltip);
 
 export default Tooltip;
