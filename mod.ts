caab775e4b23206bdf0aa0c8327f011f01a10d0e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-05-01 11:43:06 +0900

    chore: fix format

--- mod.ts
+++ mod.ts
@@ -5,6 +5,6 @@ export * from "./transition/mod.ts";
 export * from "./tab/mod.ts";
 export * from "./ssr/mod.ts";
 export * from "./tooltip/mod.ts";
-export * from './disclosure/mod.ts'
+export * from "./disclosure/mod.ts";
 export * from "./hooks/mod.ts";
 export * from "./util.ts";
