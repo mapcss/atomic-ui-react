eec3f623ee38ff2dcc2579a0ec897b1df28e7233
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-05-08 13:37:49 +0900

    feat(tab): add `render` props to `WithTabPanel` component

--- tab/with_tab_panel.ts
+++ tab/with_tab_panel.ts
@@ -8,6 +8,7 @@ import {
   ReactElement,
   Ref,
   useContext,
+  useMemo,
 } from "react";
 import { joinChars } from "../util.ts";
 import useTabPanelAria from "./use_tab_panel_aria.ts";
@@ -19,27 +20,64 @@ import {
 } from "./context.ts";
 import { PANEL, TAB } from "./constant.ts";
 
+export type RenderContext = {
+  selectedIndex: number;
+  index: number;
+  isDisabled: boolean;
+  isSelected: boolean;
+  isShowable: boolean;
+};
+export type RenderAttributes<E extends Element = Element> = {
+  children: ReactElement;
+  ref: Ref<E>;
+};
+
+export type Render<E extends Element = Element> = (
+  attributes: RenderAttributes<E>,
+  context: RenderContext,
+) => ReactElement;
+
+const defaultRender: Render = (
+  { children, ...props }: RenderAttributes,
+  { isShowable }: RenderContext,
+) => {
+  return isShowable ? cloneElement(children, props) : createElement(Fragment);
+};
+
 export type Props = {
   children: ReactElement;
+
+  render?: Render;
 };
-function _WithTabPanel<T>(
-  { children }: Props,
+function _WithTabPanel<T extends Element = Element>(
+  { children, render = defaultRender }: Props,
   ref: Ref<T>,
 ): JSX.Element {
   const id = useContext(IdContext);
   const tabPanelCount = useContext(TabPanelCountContext);
-  const [index] = useContext(IndexContext);
+  const [selectedIndex] = useContext(IndexContext);
   const disabledIds = useContext(DisabledIdsContext);
 
-  const currentIndex = tabPanelCount.current;
-  const isDisabled = disabledIds.includes(currentIndex);
-  const tabId = joinChars([id, TAB, currentIndex], "-");
-  const tabPanelId = joinChars([id, TAB, PANEL, currentIndex], "-");
+  const index = tabPanelCount.current;
+  const isDisabled = disabledIds.includes(index);
+  const isSelected = useMemo<boolean>(() => index === selectedIndex, [
+    selectedIndex,
+  ]);
+  const isShowable = useMemo<boolean>(() => isSelected && !isDisabled, [
+    isSelected,
+    isDisabled,
+  ]);
+  const tabId = joinChars([id, TAB, index], "-");
+  const tabPanelId = joinChars([id, TAB, PANEL, index], "-");
   const aria = useTabPanelAria({ tabId, tabPanelId });
 
-  return index === currentIndex && !isDisabled
-    ? cloneElement(children, { ref, ...aria })
-    : createElement(Fragment);
+  return render({ children, ref, ...aria }, {
+    selectedIndex,
+    index,
+    isDisabled,
+    isSelected,
+    isShowable,
+  });
 }
 
 const WithTabPanel = forwardRef(_WithTabPanel);
