e6f69ca9ee9335fc9184dfdd5d812ba36bdbae95
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-05-29 01:17:25 +0900

    docs(hooks): add doc comment

--- hooks/use_previous.ts
+++ hooks/use_previous.ts
@@ -12,7 +12,10 @@ import { useRef } from "react";
  * }
  * ```
  */
-export default function usePrevious<T>(value: T): T | undefined {
+export default function usePrevious<T>(
+  /** Current value. */
+  value: T,
+): T | undefined {
   const ref = useRef<T | undefined>(undefined);
   const previous = ref.current;
   ref.current = value;
