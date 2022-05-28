1aa8103c8206c3560dd5960381fbe0ce15c9c281
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-05-29 01:03:12 +0900

    chore(alert_dialog): export `AlertDialog` component

--- alert_dialog/mod.ts
+++ alert_dialog/mod.ts
@@ -11,3 +11,5 @@ export type {
   Params as UseAlertDialogParams,
   Returns as UseAlertDialogReturns,
 } from "./use_alert_dialog.ts";
+export { default as AlertDialog } from "./alert_dialog.ts";
+export type { Props as AlertDialogProps } from "./alert_dialog.ts";
