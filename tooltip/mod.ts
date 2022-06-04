050e4a8ab18d3da4d194e538739bcadcfec69737
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-04 23:18:45 +0900

    chore(tooltip): remove unused

--- tooltip/mod.ts
+++ tooltip/mod.ts
@@ -1,7 +1,4 @@
 // This module is browser compatible.
 
-export { default as WithTooltip } from "./with_tooltip.ts";
-export type { Props as WithTooltipProps } from "./with_tooltip.ts";
 export { default as TooltipProvider } from "./tooltip_provider.ts";
 export type { Props as TooltipProviderProps } from "./tooltip_provider.ts";
-export { default as Tooltip } from "./tooltip.tsx";
