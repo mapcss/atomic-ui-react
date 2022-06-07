897059c57573ea277371df85f0f3cb2b3d64e72c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-07 12:19:25 +0900

    fix(tooltip): export `Tooltip` component

--- tooltip/mod.ts
+++ tooltip/mod.ts
@@ -2,3 +2,4 @@
 
 export { default as TooltipProvider } from "./tooltip_provider.ts";
 export type { Props as TooltipProviderProps } from "./tooltip_provider.ts";
+export { default as Tooltip } from "./tooltip.ts";
