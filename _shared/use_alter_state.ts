6b4970a7ca16c219ed7f34ad9803b737039d1605
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-07 15:11:12 +0900

    chore(_shared): improve types

--- _shared/use_alter_state.ts
+++ _shared/use_alter_state.ts
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function useAlterState<T>(
-  initialState: T | (() => T),
-  stateSet?: [T, (state: T) => void],
+  initialValue: T | (() => T),
+  [state, setState]: [T | undefined, ((state: T) => void) | undefined],
 ): [T, (state: T) => void] {
-  const _stateSet = useState(initialState);
+  const _stateSet = useState(initialValue);
 
-  return stateSet ? stateSet : _stateSet;
+  return useMemo<[T, (state: T) => void]>(() => {
+    if (setState) {
+      return [state, setState] as [T, (state: T) => void];
+    } else {
+      return _stateSet;
+    }
+  }, [_stateSet[0], _stateSet[1], state, setState]);
 }
