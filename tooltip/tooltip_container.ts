75ee9bff2e24da698349773c2fd98a25f317a213
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-15 14:57:21 +0900

    refactor(tooltip): use `useStateSet` hook

--- tooltip/tooltip_container.ts
+++ tooltip/tooltip_container.ts
@@ -1,13 +1,11 @@
-import { createElement, ReactNode, useCallback } from "react";
-import { IsShowProps } from "./types.ts";
-import { Exclusive, mappingKey } from "../util.ts";
+import { createElement, Dispatch, ReactNode } from "react";
+import { Exclusive } from "../util.ts";
 import { IsShowContexts } from "./contexts.ts";
 import { Tag } from "../types.ts";
-import useAlterState from "../_shared/use_alter_state.ts";
 import useTooltipContainer from "./use_tooltip_container.ts";
 import { Contexts } from "./types.ts";
 import { AllAttributesWith } from "../hooks/mod.ts";
-import { useCallable, useEventListener } from "../hooks/mod.ts";
+import { useStateSet } from "../hooks/mod.ts";
 
 export type Props<As extends Tag> =
   & {
@@ -18,48 +16,27 @@ export type Props<As extends Tag> =
 
     children?: ReactNode;
   }
-  & Exclusive<IsShowProps, {
+  & Exclusive<{
+    /** `isShow` and it dispatcher set. */
+    isShowSet: [boolean, Dispatch<boolean>];
+  }, {
     /**
      * @default false
      */
     initialIsShow?: boolean;
   }>
   & Partial<AllAttributesWith<[Contexts]>>;
+
 export default function TooltipContainer<As extends Tag = "div">(
   {
     as = "div" as As,
     children,
     initialIsShow = false,
-    setIsShow: setState,
-    isShow: state,
+    isShowSet,
     ...allAttributes
   }: Readonly<Props<As>>,
 ): JSX.Element {
-  const [isShow, setIsShow] = useAlterState<boolean>(initialIsShow, [
-    state,
-    setState,
-  ]);
-
-  const target = useCallback<() => Document>(() => document, []);
-  const keyboardHandler = useCallback((ev: KeyboardEvent) => {
-    const runner = mappingKey([
-      ["Escape", (ev) => {
-        ev.preventDefault();
-        setIsShow(false);
-      }],
-    ]);
-    runner(ev);
-  }, [setIsShow]);
-  const callback = useCallable(keyboardHandler, isShow);
-
-  useEventListener(
-    {
-      target,
-      event: "keydown",
-      callback,
-    },
-    [target, callback],
-  );
+  const [isShow, setIsShow] = useStateSet<boolean>(initialIsShow, isShowSet);
 
   const [attributes] = useTooltipContainer(
     { isShow, setIsShow },
@@ -68,12 +45,7 @@ export default function TooltipContainer<As extends Tag = "div">(
 
   return createElement(
     IsShowContexts.Provider,
-    {
-      value: {
-        isShow,
-        setIsShow,
-      },
-    },
+    { value: { isShow, setIsShow } },
     createElement(as, attributes, children),
   );
 }
--- tooltip/use_tooltip_container.ts
+++ tooltip/use_tooltip_container.ts
@@ -1,14 +1,38 @@
+import { useCallback } from "react";
 import { AllAttributesWith, useAttributesWith } from "../hooks/mod.ts";
 import { Contexts } from "./types.ts";
+import { mappingKey } from "../util.ts";
+import { useCallable, useEventListener } from "../hooks/mod.ts";
 
 export default function useTooltipContainer(
-  contexts: Readonly<Contexts>,
+  ctx: Readonly<Contexts>,
   allAttributes: Partial<AllAttributesWith<[Contexts]>> = {},
 ) {
-  const attributes = useAttributesWith<[Contexts]>([contexts], {
+  const attributes = useAttributesWith<[Contexts]>([ctx], {
     ...defaultAttributes,
     ...allAttributes,
   });
+
+  const target = useCallback<() => Document>(() => document, []);
+  const keyboardHandler = useCallback((ev: KeyboardEvent) => {
+    const runner = mappingKey([
+      ["Escape", (ev) => {
+        ev.preventDefault();
+        ctx.setIsShow(false);
+      }],
+    ]);
+    runner(ev);
+  }, [ctx.setIsShow]);
+  const callback = useCallable(keyboardHandler, ctx.isShow);
+
+  useEventListener(
+    {
+      target,
+      event: "keydown",
+      callback,
+    },
+    [target, callback],
+  );
 
   return [attributes];
 }
